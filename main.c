81ac23a7aa19404c78b9c3515c65dc4ef2510633
MikiyaShibuya <mikiya.mail4univ@gmail.com>
2023-11-12 22:11:06 +0900

    Update source

--- main.c
+++ main.c
@@ -9,24 +9,24 @@
 
 // CONFIG1
 #pragma config FOSC = INTOSC     // Use internal clock
-#pragma config WDTE = OFF            // Disable watch dog timer
-#pragma config PWRTE = ON            // Enable power-up timer
-#pragma config MCLRE = OFF         // Use MCLR pin as RA3
-#pragma config CP = OFF                // Don't protect program memory
-#pragma config BOREN = ON            // Enable brown out reset
+#pragma config WDTE = OFF        // Disable watch dog timer
+#pragma config PWRTE = ON        // Enable power-up timer
+#pragma config MCLRE = OFF       // Use MCLR pin as RA3
+#pragma config CP = OFF          // Don't protect program memory
+#pragma config BOREN = ON        // Enable brown out reset
 #pragma config CLKOUTEN = OFF    // Don't use clock out, use as RA4
-#pragma config IESO = OFF            // Disable 2-stage clocks start up
-#pragma config FCMEN = OFF         // Don't monitor clock
+#pragma config IESO = OFF        // Disable 2-stage clocks start up
+#pragma config FCMEN = OFF       // Don't monitor clock
 
 // CONFIG2
-#pragma config WRT = OFF             // Don't protect flash memeory
+#pragma config WRT = OFF         // Don't protect flash memeory
 #pragma config PPS1WAY = OFF     // Allow to try to unlock PPSLOCK anytime
-#pragma config ZCDDIS = ON         // Disable zero cross detection
-#pragma config PLLEN = OFF         // Disable ×4PLL
-#pragma config STVREN = ON         // Enable stack overflow reset
-#pragma config BORV = HI             // Set brown out trigger voltage as high
-#pragma config LPBOR = OFF         // Disable low power brown out reset
-#pragma config LVP = OFF             // Disable low voltage programming
+#pragma config ZCDDIS = ON       // Disable zero cross detection
+#pragma config PLLEN = OFF       // Disable ×4PLL
+#pragma config STVREN = ON       // Enable stack overflow reset
+#pragma config BORV = HI         // Set brown out trigger voltage as high
+#pragma config LPBOR = OFF       // Disable low power brown out reset
+#pragma config LVP = OFF         // Disable low voltage programming
 
 #define _XTAL_FREQ 8000000
 
@@ -36,24 +36,30 @@ typedef unsigned char uint8_t;
 // typedef unsigned int uint16_t; You can use this type
 typedef unsigned long uint32_t;
 
+// Count of 1 day
 const static uint32_t COUNT_1DAY = 86400 * 1024;
+// Initial value of countdown timer to measure 1/1024 sec
 const static int TMR_CNT_H = (65536 - 235) >> 8;
 const static int TMR_CNT_L = (65536 - 235) & 0xff;
 
 // Time to keep powered when not moving
 const static uint32_t STATIONAL_COUNT = 3L * 60 * 1024; // 3 min
-// const static uint32_t STATIONAL_COUNT = 3L * 1024; // 3 sec
 
 // Duty ratio is [DUTY VALUE] / 124
 const static char METER_LAMP_DUTY = 15;
-const static char MAIN_LIGHT_DUTY = 80;
-const static char TAIL_LAMP_DIM_DUTY = 25;
-const static char TAIL_LAMP_FLASH_DUTY = 124;
-const static int BLIGHT_DURATION = 60;
+const static char HEADLIGHT_DUTY = 80;
+const static char HEADLIGHT_DAY_DUTY = 15;
+const static char TAIL_LAMP_DIM_DUTY = 10;
+const static char TAIL_LAMP_FLASH_DUTY = 40;
+const static int BRIGHT_DURATION = 60;
 
-// Count 1024th of 1 sec since vibration sensor change, max 86400 * 1024 = 1day
+// Brightness thresholds
+const static int AMB_BRIGHT_THR_LOW = 500;
+const static int AMB_BRIGHT_THR_HIGH = 800;
+
+// Count 1024th of 1 sec since vibration sensor stopped, max 86400 * 1024 = 1day
 volatile static uint32_t count;
-// Count 1024th of 1 sec since chip was powered
+// Millisecond count (1/1024 sec in precisely) since when chip was powered
 volatile static uint16_t millis;
 
 // Get analog voltage as a floating point value in 0 to 1023
@@ -145,52 +151,67 @@ void main() {
     PWM4DCH = 0;
     PWM4DCL = 0;
 
-    T2CON = 0b00000110; // Turn on TMR2 with x16 prescaler
+    T2CON = 0b00000110; // Turn on TMR2 with x16 pre-scaler
 
     char vib_sns_prev = read_adc(0) > 50 ? 1 : 0; // Read from RA4
-    int adc_value_itg = 512;
+    int adc_value_itg = 0;
 
-    // Produce third PWM by simply on/off;
+    // Produce third PWM in main loop, this is used for meter lamp;
     int pwm_count = 0;
     int pwm_duty = 1;
     int pwm_period = 8;
+    
+    char is_amb_dark = 1;
 
     while(1) {
+        // Check ambient brightness as frequent as PWM
         if (pwm_count == 0) {
             // Read analog value from RC2 (0 - 1023)
             int adc_value_read = read_adc(1);
+            // Filter
             adc_value_itg = adc_value_itg - (adc_value_itg >> 3) + (adc_value_read >> 3);
         }
 
-        char is_dark = adc_value_itg < 512 ? 1 : 0;
-
-        char vib_sns = read_adc(0) > 50 ? 1 : 0;
-        if (count > 102) {
-              // Reset counter
-              if (vib_sns_prev != vib_sns && count > 102) {
-                  vib_sns_prev = vib_sns;
-                  restart_counter();
-              }
+        if (is_amb_dark == 1 && adc_value_itg > AMB_BRIGHT_THR_HIGH) {
+            is_amb_dark = 0;
+        }
+        else if (is_amb_dark == 0 && adc_value_itg <= AMB_BRIGHT_THR_LOW) {
+            is_amb_dark = 1;
         }
 
+        // Check vibration every 1/8 sec
+        if (count % 128 == 0) {
+            char vib_sns = read_adc(0) > 50 ? 1 : 0;
+            // Reset counter
+            if (vib_sns_prev != vib_sns) {
+                vib_sns_prev = vib_sns;
+                restart_counter();
+            }
+        }
+
+        // Turn off all lights when the bike keeps be stational for a while
         if (count > STATIONAL_COUNT) {
             PWR_EN = 0;
             PWM3DCH = 0;
             PWM4DCH = 0;
         }
         else {
+            // Keep turn on main power switch
             PWR_EN = 1;
-            if (is_dark) {
-                PWM3DCH = MAIN_LIGHT_DUTY;
+            
+            // Turn on headlight when the ambient light is dark
+            if (is_amb_dark) {
+                PWM3DCH = HEADLIGHT_DUTY;
             }
             else{
-                    PWM3DCH = 0;
+                PWM3DCH = HEADLIGHT_DAY_DUTY;
             }
 
-            if (millis < BLIGHT_DURATION) {
+            // Flash tail lamp
+            if (millis < BRIGHT_DURATION) {
                 PWM4DCH = TAIL_LAMP_FLASH_DUTY;
             }
-            else if (100 <= millis && millis < 100 + BLIGHT_DURATION) {
+            else if (100 <= millis && millis < 100 + BRIGHT_DURATION) {
                 PWM4DCH = TAIL_LAMP_FLASH_DUTY;
             }
             else {
@@ -198,7 +219,7 @@ void main() {
             }
         }
 
-        if (pwm_count < pwm_duty && is_dark) {
+        if (pwm_count < pwm_duty && is_amb_dark) {
                 RC3 = 1;
         }
         else {
