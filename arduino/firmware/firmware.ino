2cf9ae85758854a6dd294aa085352b445a44d3f1
MikiyaShibuya <mikiya.mail4univ@gmail.com>
2024-12-06 16:51:05 +0900

    Implement day/night switching using ambient light sensor and optimize program

--- arduino/firmware/firmware.ino
+++ arduino/firmware/firmware.ino
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 
-// Pin configuration
+// ====  Pin configuration ====
 #define PIN_VIB_SNS 0
 #define PIN_AMB_SNS 1
 #define PIN_PW_CTL 4
@@ -8,34 +8,62 @@
 #define PIN_HL_LED 6
 #define PIN_TL_LED 7
 
-// Parameters
-#define MAX_CNT 20        // timeout in second
-#define VIB_THRESHOLD 100 // 0-1023, lower is more sensitive
-#define BLINK_PERIOD_1 60
-#define BLINK_PERIOD_2 40
-#define BLINK_PERIOD_3 60
-#define BLINK_PERIOD_4 840
+// ==== Parameters ====
 
+// Timeout
+#define MAX_CNT 180 // timeout in second
+
+// Sensor
+#define VIB_THRESHOLD 100 // 0-1023, lower is more sensitive
+// 0-1023, lower is darker
+#define AMB_THRESHOLD_NIGHT 700 // It's night if ambient light is below this
+#define AMB_THRESHOLD_DAY 850   // It's day if ambient light is above this
+
+// Flash control
+#define BLINK_PERIOD_1 60  // FLASH
+#define BLINK_PERIOD_2 40  // DIM
+#define BLINK_PERIOD_3 60  // FLASH
+#define BLINK_PERIOD_4 840 // DIM
+
+// Brightness
+// Meter brightness is constant
 #define METER_BRIGHTNESS 6
+// Headlight brightness is different for day and night
 #define HEADLIGHT_DAY_BRIGHTNESS 6
 #define HEADLIGHT_NIGHT_BRIGHTNESS 50
+// Taillight brightness is not changed in day or night but it blinks
 #define TAILLIGHT_BASE_BRIGHTNESS 8
 #define TAILLIGHT_BRIGHTNESS 30
+
+bool is_dark = false;
 
 // This is working on 20MHz
 // 22.7us per adc read
 // (5 * 1000 * 100 times adc read took 11.35s)
 
-// 5 * 40 * 1000 = 6.35s
+void wait_ms(int ms, int *cnt, bool drive_light) {
 
-void wait_ms(int ms, int *cnt) {
-
-  ms = ms * 0.96; // Cancel out adc sampling delay
+  ms = ms * 0.94; // Cancel out adc sampling delay
 
   for (int i = 0; i < ms; i++) {
-    // 31.75us per loop
+    // 51.7us per loop
     {
+      if (drive_light) {
+        const int amb = analogRead(PIN_AMB_SNS); // 0-1023
+        if (is_dark) {
+          is_dark = amb < AMB_THRESHOLD_DAY;
+        } else {
+          is_dark = amb < AMB_THRESHOLD_NIGHT;
+        }
+        if (is_dark) {
+          analogWrite(PIN_HL_LED, HEADLIGHT_NIGHT_BRIGHTNESS);
+        } else {
+          analogWrite(PIN_HL_LED, HEADLIGHT_DAY_BRIGHTNESS);
+        }
+      }
+
       const int val = analogRead(PIN_VIB_SNS); // 0-1023
+
       if (val > VIB_THRESHOLD) {
         *cnt = 0;
       }
@@ -56,37 +84,47 @@ void setup() {
 
   int cnt = 0;
 
-  analogWrite(PIN_HL_LED, 127);
-
-  wait_ms(100, &cnt);
+  analogWrite(PIN_HL_LED, HEADLIGHT_NIGHT_BRIGHTNESS);
+  delay(60);
+  analogWrite(PIN_HL_LED, HEADLIGHT_DAY_BRIGHTNESS);
+  delay(40);
+  analogWrite(PIN_HL_LED, HEADLIGHT_NIGHT_BRIGHTNESS);
+  delay(60);
+  analogWrite(PIN_HL_LED, HEADLIGHT_DAY_BRIGHTNESS);
+  delay(200);
 
   while (true) {
-    const bool is_dark = true; // TODO: read from ambient sensor
-    if (is_dark) {
-      analogWrite(PIN_HL_LED, HEADLIGHT_NIGHT_BRIGHTNESS);
-    } else {
-      analogWrite(PIN_HL_LED, HEADLIGHT_DAY_BRIGHTNESS);
-    }
+    // Setup meter light
     analogWrite(PIN_MT_LED, METER_BRIGHTNESS);
 
+    // Blink tail light
     analogWrite(PIN_TL_LED, TAILLIGHT_BRIGHTNESS);
-    wait_ms(BLINK_PERIOD_1, &cnt);
+    wait_ms(BLINK_PERIOD_1, &cnt, true);
     analogWrite(PIN_TL_LED, TAILLIGHT_BASE_BRIGHTNESS);
-    wait_ms(BLINK_PERIOD_2, &cnt);
+    wait_ms(BLINK_PERIOD_2, &cnt, true);
     analogWrite(PIN_TL_LED, TAILLIGHT_BRIGHTNESS);
-    wait_ms(BLINK_PERIOD_3, &cnt);
+    wait_ms(BLINK_PERIOD_3, &cnt, true);
     analogWrite(PIN_TL_LED, TAILLIGHT_BASE_BRIGHTNESS);
-    wait_ms(BLINK_PERIOD_4, &cnt);
+    wait_ms(BLINK_PERIOD_4, &cnt, true);
+
+    // Check timeout
     cnt++;
     if (cnt >= MAX_CNT) {
-      break;
+      analogWrite(PIN_MT_LED, 0);
+      analogWrite(PIN_HL_LED, 0);
+      analogWrite(PIN_TL_LED, 0);
+
+      // Wair for vibration
+      while (true) {
+        digitalWrite(PIN_PW_CTL, LOW);
+
+        wait_ms(100, &cnt, false);
+        if (cnt == 0) {
+          break;
+        }
+      }
     }
   }
-
-  analogWrite(PIN_MT_LED, 0);
-  analogWrite(PIN_HL_LED, 0);
-  analogWrite(PIN_TL_LED, 0);
-  digitalWrite(PIN_PW_CTL, LOW);
 }
 
 void loop() {}
